b0c381d3790e80288b4a8e8335adc3165cb82517
bendenko-v <bendenko.v@gmail.com>
2023-08-28 17:45:43 +0200

    Added docstrings in crud

--- src/crud/crud_employee.py
+++ src/crud/crud_employee.py
@@ -1,22 +1,35 @@
 from sqlalchemy import func
 from sqlalchemy.orm import Session
 
-from ..crud.base import BaseCRUD
-from ..models.employee import EmployeeModel
-from ..models.task import StatusEnum, TasksModel
-from ..schemas.employee import EmployeeCreateScheme
+from src.crud.base import BaseCRUD
+from src.models.employee import EmployeeModel
+from src.models.task import StatusEnum, TasksModel
+from src.schemas.employee import EmployeeCreateScheme
 
 
 class EmployeeCRUD(BaseCRUD[EmployeeModel, EmployeeCreateScheme]):
     """
-    CRUD for employees
+    CRUD operations for employees.
+
+    Args:
+        BaseCRUD (type): The base CRUD class for performing common operations.
     """
 
     async def get_employee_by_name(self, db: Session, employee_name: str) -> EmployeeModel:
+        """ Retrieve an employee by their name """
         return db.query(EmployeeModel).filter(EmployeeModel.name == employee_name).first()
 
     async def busiest_employee(self, db: Session) -> list[tuple[EmployeeModel, ...] | None]:
-        # Get employees with active tasks (status = doing) in descending order """
+        """
+        Retrieve a list of employees with active tasks (status = doing) in descending order.
+
+        Args:
+            db (Session): The SQLAlchemy session.
+
+        Returns:
+            list[tuple[EmployeeModel, ...] | None]:
+                A list of tuples containing employee instances and their active task counts.
+        """
         employees = (
             db.query(EmployeeModel, func.count(TasksModel.id).label('active_task_count'))
             .outerjoin(TasksModel, TasksModel.employee_id == EmployeeModel.id)
@@ -27,7 +40,16 @@ class EmployeeCRUD(BaseCRUD[EmployeeModel, EmployeeCreateScheme]):
         return employees
 
     async def less_busy_employee(self, db: Session) -> list[tuple[EmployeeModel, ...] | None]:
-        # Get employees with active tasks in ascending order """
+        """
+        Retrieve a list of employees with active tasks in ascending order.
+
+        Args:
+            db (Session): The SQLAlchemy session.
+
+        Returns:
+            list[tuple[EmployeeModel, ...] | None]:
+                A list of tuples containing employee instances and their task counts.
+        """
         employees = (
             db.query(EmployeeModel, func.count(TasksModel.id).label('task_count'))
             .outerjoin(TasksModel, EmployeeModel.id == TasksModel.employee_id)
--- src/crud/crud_task.py
+++ src/crud/crud_task.py
@@ -7,11 +7,22 @@ from src.schemas import TaskCreateScheme
 
 class TaskCRUD(BaseCRUD[TasksModel, TaskCreateScheme]):
     """
-    CRUD for tasks
+    CRUD operations for tasks.
+
+    Args:
+        BaseCRUD (type): The base CRUD class for performing common operations.
     """
 
     async def get_important_tasks(self, db: Session) -> list[TasksModel | None]:
-        # Get important tasks (tasks has parent and no employee assigned) """
+        """
+        Retrieve a list of important tasks that have no assigned employee but have a parent task.
+
+        Args:
+            db (Session): The SQLAlchemy session.
+
+        Returns:
+            list[TasksModel | None]: A list of important task instances, or None if no such tasks are found.
+        """
         tasks = (db.query(TasksModel).filter(TasksModel.employee_id == None, TasksModel.parent_id != None)).all()
         return tasks
 
